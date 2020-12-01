f210cea7bcc46789f53e77af8edd1ec3e9921911
krit <kinonta@gmail.com>
2020-12-01 17:00:02 +0700

    basic

--- src/Application.kt
+++ src/Application.kt
@@ -3,11 +3,22 @@ package com.thankrit
 import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.request.*
+import io.ktor.routing.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
-@Suppress("unused") // Referenced in application.conf
-@kotlin.jvm.JvmOverloads
-fun Application.module(testing: Boolean = false) {
+// ipconfig
+fun Application.module() {
+
+    install(Routing){
+        get("/api/krit/test") {
+            call.respond("dru sp")
+        }
+
+        post("/api/krit/testtest"){
+            call.respond("OK success")
+        }
+    }
+
 }
 
