a46b0c8aa2193b9ea8cd635b2830da8d4e491b58
krit <kinonta@gmail.com>
2020-12-01 17:55:31 +0700

    user

--- src/Application.kt
+++ src/Application.kt
@@ -4,7 +4,6 @@ import io.ktor.application.*
 import io.ktor.features.*
 import io.ktor.gson.*
 import io.ktor.response.*
-import io.ktor.request.*
 import io.ktor.routing.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
@@ -35,7 +34,23 @@ fun Application.module() {
             val response = """{"name": "Kate", "pet": {"dog": "Corgi", "cat": "Persian"}}"""
             call.respond(response)
         }
+
+        get("/api/krit/user") {
+            val user = User(name = "krit", age = 21)
+            val response = UserResponse(success = true, message = "User success", user = user)
+            call.respond(response)
+        }
     }
 
 }
 
+data class UserResponse(
+    val success: Boolean = false,
+    val message: String? = null,
+    val user: User? = null,
+)
+
+data class User(
+    val name: String? = null,
+    val age: Int? = null,
+)
